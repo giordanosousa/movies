2d6b20f24d5bee377926d1b2e2cc163114605ba4
Giordano Sousa <giordanosousa.dev@gmail.com>
2024-03-19 15:56:18 -0300

    primeiro commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>GiordFlix &copy; Desenvolvido por Giordano Sousa 2024</h2>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,31 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Banner from "../../components/Banner";
+import Container from "../../components/Container";
+import Card from "../../components/Card";
+import Category, {categories, filterCategory} from "../../components/Category";
+import Carousel from "../../components/Carousel";
+
+function Home() {
+
+
+  return (
+   <>
+      <Header />
+      <Banner image="favoritos" />
+      <Container>
+        { categories.map((category, index) => 
+          <Category category={category}>
+            <Carousel>
+              { filterCategory(index).map((video) => <Card id={video.id}
+              key={video.id} /> )}
+            </Carousel>
+          </Category>)
+        }
+      </Container>
+      <Footer />
+   </>
+  );
+}
+
+export default Home;
